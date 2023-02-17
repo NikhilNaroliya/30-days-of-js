d451159cb8f4ee4c1407c8538b6dee96e17499b9
nikhil naroliya <95118417+NikhilNaroliya@users.noreply.github.com>
2023-02-17 14:14:06 +0530

    Update index.js
    added neog set 1 solutions

--- challanges/index.js
+++ challanges/index.js
@@ -152,10 +152,10 @@
 //   }
 
 
-                    
-                            //string challenge
 
-              // function to  convert underscore to camelCase
+//string challenge
+
+// function to  convert underscore to camelCase
 
 // nikhil_naroliya to nikhilNaroliya
 //  function tocameCase(str,i)
@@ -198,21 +198,21 @@
 //      }
 
 
-           // challenge to user input from user throgh prompt and  increase the index value based on user input
-          
-           
-            // const poll={
-            //   question:'what is your fav prog language ?',
-            //   option:['0:js','1:python','2:cpp','3:rust'],
+// challenge to user input from user throgh prompt and  increase the index value based on user input
 
-            //   answers:new Array(4).fill(0),
 
-            //   registerNewAns(){
-            //     const answer=Number(prompt(`${this.question}\n ${this.option.join('\n)}`
-            //   }
+// const poll={
+//   question:'what is your fav prog language ?',
+//   option:['0:js','1:python','2:cpp','3:rust'],
 
-            // }
-            
+//   answers:new Array(4).fill(0),
+
+//   registerNewAns(){
+//     const answer=Number(prompt(`${this.question}\n ${this.option.join('\n)}`
+//   }
+
+// }
+
 
 // Coding Challenge #2
 
@@ -232,46 +232,46 @@ GOOD LUCK 😀
 //   });
 // })();
 
-            // <-----coding challenge from arrays section------>
+// <-----coding challenge from arrays section------>
 
 
-    // const Juliaowner1=[1,2,2,5,3,5,4];
-    // const Kateowner2=[2,2,5,5,1,6,7];
+// let Juliaowner1=[1,2,2,5,3,5,4];
+// const Kateowner2=[2,2,5,5,1,6,7];
 
-    //  const Juliaowner2=Juliaowner1;  /// created a copy
-      
-    //  console.log(Juliaowner1);
-    //  Juliaowner2.splice(0,1)   // after 0 delete 1 element
-    //  Juliaowner2.splice(-2);
+//  const Juliaowner2=Juliaowner1;  /// created a copy
 
-    //  console.log(Juliaowner2);
+//  console.log(Juliaowner1);
+//  Juliaowner2.splice(0,1)   // after 0 delete 1 element
+//  Juliaowner2.splice(-2);
 
-    //   function checkDogsAge(Juliaowner1,Kateowner2)
-    //  {
-    //         Juliaowner2.forEach(element => {
-    //            if(element>3)
-    //             {
-    //               console.log('the dog is adult')
-    //             }
-    //             else
-    //              console.log('the dog is still a  puppy')
-    //             });
+//  console.log(Juliaowner2);
 
-    //            console.log('--------For kate----------') 
-    //            Kateowner2.forEach(element => {
-    //             if(element>3)
-    //              {
-    //                console.log('the dog is adult')
-    //              }
-    //              else
-    //               console.log('the dog is still a  puppy')
-    //              });
-    //  }
+//   function checkDogsAge(Juliaowner1,Kateowner2)
+//  {
+//         Juliaowner2.forEach(element => {
+//            if(element>3)
+//             {
+//               console.log('the dog is adult')
+//             }
+//             else
+//              console.log('the dog is still a  puppy')
+//             });
 
-    //  checkDogsAge(Juliaowner2,Kateowner2);
+//            console.log('--------For kate----------') 
+//            Kateowner2.forEach(element => {
+//             if(element>3)
+//              {
+//                console.log('the dog is adult')
+//              }
+//              else
+//               console.log('the dog is still a  puppy')
+//              });
+//  }
 
-    //   <-----------array challenge dog ->human age ------------->
-       
+//  checkDogsAge(Juliaowner2,Kateowner2);
+
+//   <-----------array challenge dog ->human age ------------->
+
 /* 
 Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
 Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
@@ -283,19 +283,223 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
-    const calcAverageHumanAge = function (ages) {
-      const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-      const adults = humanAges.filter(age => age >= 18);
-      console.log(humanAges);
-      console.log(adults);
-      // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
-      const average = adults.reduce(
-        (acc, age, i, arr) => acc + age / arr.length,
-        0
-      );
-      // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
-      return average;
-    };
-    const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-    const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-    console.log(avg1, avg2);
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+//   console.log(humanAges);
+//   console.log(adults);
+//   // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+//   const average = adults.reduce(
+//     (acc, age, i, arr) => acc + age / arr.length,
+//     0
+//   );
+//   // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
+//   return average;
+// };
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+// console.log(avg1, avg2);
+
+
+
+// <---------------challenge number 4---------------->
+//                      1
+
+
+//     let Juliaowner1=[1,2,2,5,3,5,4];
+//     const Kateowner2=[2,2,5,9,1,6,7];
+
+//          Juliaowner1=Juliaowner1.map(ele=>(ele**0.75*28)/1000)
+//           console.log(Juliaowner1)
+
+//           //                  2
+
+//           //let say sarah dog is 9 one and its eating lot
+
+//            // Method (return element > 0).
+//     var found = Kateowner2.find(function (element) {
+//       return element ==9;
+//   });
+
+//    console.log(found)
+
+//----------------------NEOG PRACTICE--------------
+//SET-1
+
+// -------ques -1------
+
+//  function oldEnough(num){
+//     if(num>18)console.log(`eligible to vote`)
+//     else 
+//     console.log(`not eligible to vote`)
+//  }
+//  console.log(oldEnough(16));
+//  console.log(oldEnough(45))
+
+//----------ques2----------
+
+// function isGreater(num1,num2)
+// {
+//    num1>num2?console.log(`${num1} is greater`):console.log(`${num2} is greater`)
+
+// }
+// isGreater(2,3)
+
+//-----------ques 3---------
+
+//    function pos_neg(num){
+//     if(num>0)console.log(`it  is +ve`)
+//     else
+//     console.log(`it is -ve`);
+//    }
+
+//    pos_neg(-3)
+
+// -------------ques 4---------
+// function even_odd(num)
+//  {
+//     if(num%2==0)
+//      console.log(` it is even `);
+//      else 
+//       console.log(` it  is odd `)
+//  }
+//   even_odd(34)
+
+//-------------ques 5--------------
+
+//   function Contains_a_A(str)
+//    {
+//       if(str.includes('a'))
+//       { console.log(' it is having a ');
+//        if(str.includes('A'))
+//        console.log('it is having A')
+//    }
+//        else
+//        console.log('it does not contain a || A')
+
+//    }
+//    Contains_a_A('neog')
+
+//---------------------ques 6--------------
+
+//  const str='nikhil';
+//  console.log(str.length)
+
+//   const getLenght=str=>
+
+//   {  if(str.length>5)
+//      console.log('greater than 5')
+//       else
+//        console.log('lesser than 5')
+//   }
+//   getLenght('nik')
+
+//---------------ques --------------------
+
+// const isBetWeen_1_to_10=num=>{
+//     if(num>=1 && num<=10)
+//      {
+//         console.log(`${num} is between 1 to 10`);
+
+//      }
+//      else console.log(num+' is not between 1 to 10')
+// }
+// isBetWeen_1_to_10(33)
+
+//---------------ques 8------------
+
+//    function Contains_a_A(str)
+//        {
+//           if(str.includes('hello'))
+//           { console.log(' it is having  ');
+//            if(str.includes('Hello'))
+//            console.log('it is having ')
+//        }
+//            else
+//            console.log('it does not contain ')
+
+//        }
+//        Contains_a_A('hello')
+
+// -------------ques 9------------
+
+
+//        function multiple_of_3(num)
+//  {
+//     if(num%3==0)
+//      console.log(` it is multiple of 3 `);
+//      else 
+//       console.log(` it  is not multiple of 3 `)
+//  }
+//   multiple_of_3(33)
+
+
+//-------------ques 10--------------
+
+//  const multiply_By_10=num=>{
+//    return  num*10
+
+//  }
+//  console.log(multiply_By_10(3));
+
+//--------------ques 11------------
+
+// const product = {
+//     title: "iPhone",
+//     price: 5999,
+//     description: "The iPhone is a smartphone developed by Apple"
+//   }
+//   console.log(product.title); // iPhone
+//   console.log(product.price); // 999
+//   console.log(product.description);
+
+//------------ques 12----------------
+
+//    book={
+//         author:"nikhil",
+//         title:"the snake crucified",
+//         page:101,
+//     }
+//     function getBookDetails(obj)
+//      {
+//         if(obj.page>100)
+//          console.log('Having pages greater than 100')
+//          else console.log('having less than 100 pages')
+//      }
+//      getBookDetails(book)
+
+// --------------------ques 13------------
+
+                        // person = {
+                        //     name: "nikhil",
+                        //     occupation: "writer",
+                        //     age: 23,
+
+                        // }
+                        // function changeOccupation(obj, newOccupation) {
+                        //     obj.occupation = newOccupation;
+
+                        // }
+
+                        // console.log(person); // logs { name: 'Amit', age: 25, occupation: 'Software Engineer' } to the console
+                        // changeOccupation(person, 'Product Manager');
+                        // console.log(person); 
+                        
+//------------------ques 14-------------------
+// const numbers = [1, 2, 3];
+// // Your ES6 code here
+//  const [num1,num2,num3]=numbers
+
+
+// console.log(num1); // logs 1 to the console
+// console.log(num2); // logs 2 to the console
+// console.log(num3); // logs 3 to the console
+
+//  --------------------ques 15--------------
+
+// const defaultParamsFunc=(a, b, c=4)=> {
+
+//     return a * b * c;
+// 	};
+// 	console.log(defaultParamsFunc(3, 1)); // 12
+// 	console.log(defaultParamsFunc(3, 10)); // 120
