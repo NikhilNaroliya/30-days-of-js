4a08fc91e1c5117793500a35ff24d2049af77386
nikhil naroliya <95118417+NikhilNaroliya@users.noreply.github.com>
2023-02-17 16:27:47 +0530

    Update index.js
    added set 2 solutions

--- challanges/index.js
+++ challanges/index.js
@@ -503,3 +503,218 @@ GOOD LUCK 😀
 // 	};
 // 	console.log(defaultParamsFunc(3, 1)); // 12
 // 	console.log(defaultParamsFunc(3, 10)); // 120
+
+             //-------------------set 2-----------------
+
+     // -------------ques 1----------   
+        //    Create an object person with two properties, "name" and "age" and then updates the "age" property to a new value. Initial age should be 30.
+
+    // const person={
+    //     name:'nikhil',
+    //     age:30,
+    // }
+
+    //  person.age=23;
+    //  console.log(person.age)
+
+    //--------------ques 2----------
+    // Write a function that takes an object car and returns true if the car is a sports car (i.e. has a horsepower property greater than or equal to 300)
+
+//     function isSportsCar(obj)
+//      {
+//         if(obj.horsepower>300)
+//         return console.log(` ${obj.make} is a sports car`)
+//         else
+//        return console.log(` ${obj.make} is  not  a sports car`)
+         
+//      }
+
+
+// const car1 = { make: 'Porsche', model: '911', horsepower: 450 };
+// const car2 = { make: 'Toyota', model: 'Camry', horsepower: 200 };
+// isSportsCar(car1); // true
+// isSportsCar(car2); // false
+
+     //-----------------ques 3-------
+    // Write a function that takes an object person and a number num as arguments and returns true if the person's age plus num is greater than or equal to 21. Otherwise, it should return false.
+     
+//Your ES6 code here
+
+//  function isEligible(obj,additionalAge)
+//   {
+//     if(obj.age+additionalAge>21)
+//       return true;
+//       else
+//       return false;
+//   }
+// const person1 = { name: 'Ajay', age: 20 };
+// console.log(isEligible(person1, 1)); // false
+// console.log(isEligible(person1, 2)); // true
+
+//-----------------ques 4---------------
+  //Write a function that takes an object blog and returns true if the blog has more than 1000 views (i.e. has a views property greater than 1000)
+                // function getViews(blog){
+                //     if(blog.views>1000)return true;
+
+                //     return false
+                // }
+                
+                // const blog1 = {title: 'How to Learn JavaScript', author: 'John Doe', views: 1430};
+                // const blog2 = {title: '10 Reasons to Start a Blog', author: 'Jane Smith', views: 500};
+                // console.log(getViews(blog1)); // true
+                // console.log(getViews(blog2)); // false
+
+       //---------------ques 5-------------------
+    //    Swap the values of two variables using array destructuring.
+
+//        let a = 1;
+// let b = 2;
+// // Your ES6 Code here
+// [a,b]=[b,a]
+
+
+// console.log(a) // 2
+// console.log(b) // 1
+
+ //----------------------------ques 6--------------
+    // Convert this function into ES6 with least amount of characters.
+    // function add(a = 30, b = 0) {
+// 	  return a + b;
+// 	}
+
+// console.log(add(2, 3));
+
+//                            solution--------------------
+// const add=(a = 30, b = 0) => a + b;
+  
+
+// console.log(add(2, 3));
+
+// -----------------ques 7--------------
+
+// Write an ES6 function combineObjects with least amount of characters which merges two objects into one.
+
+// Your ES6 function here
+
+// function combineObjects(ob1,ob2)
+//  {
+//    const combineObj={...ob1,...ob2}
+//    return combineObj
+//  }
+
+// const obj1 = {a: 1, b: 2};
+// const obj2 = {c: 3, d: 4};
+// const combinedObj = combineObjects(obj1, obj2);
+// console.log(combinedObj);
+// // Expected Output: {a: 1, b: 2, c: 3, d: 4}
+
+//----------------------ques 8-----------------
+// Convert the function getData, into an ES6 function with least amount of characters. 
+
+// function getData(person) {
+//     const name = person.name;
+// 		const address = person.address.city
+//     console.log(name); // John Doe
+//     console.log(address); // New York
+// }
+
+// const person = {
+//   name: 'John Doe',
+//   address: {
+//     city: 'New York',
+//     state: 'NY',
+//   },
+// };
+// getData(person);
+
+//           --sol-- 
+
+// const getData=(person)=> {
+//     [ name1,address] =[ person.name,person.address.city];
+	
+//     console.log(`${name1}\n${address}`); // John Doe
+//                                         // New York
+// }
+
+// const person = {
+//   name: 'John Doe',
+//   address: {
+//     city: 'New York',
+//     state: 'NY',
+//   },
+// };
+// getData(person);
+
+
+//          --ques 9------ 
+
+// 9. Write a function that takes a string as input and returns the string in all uppercase letters.
+
+// Youe ES6 code here
+
+// function stringToUpperCase(str)
+//  {
+//     return str.toUpperCase()
+//  }
+
+// console.log(stringToUpperCase("hello")); // "HELLO"
+
+//----------------------ques 10--------------
+// Write a function that takes two strings as input and concatenates them together.
+
+// Your ES6 code here
+
+// const concatenateStrings=(str1,str2)=>str1+str2;
+
+// console.log(concatenateStrings("hello", "world")); // "helloworld"
+
+//----------------------ques 11---------------
+// Write a function that takes an array and returns the last element in the array.
+
+// Your ES6 code here
+
+// const lastElement=(arr=[])=>{
+//     return arr[arr.length-1]
+// }
+// console.log(lastElement([1, 2, 3, 4, 5])); // 5
+
+
+// -----------------ques 12----------
+// Write a function that takes an array and returns the first element of the array.
+
+// Your ES6 code here
+// const firstElement=(arr=[])=>{
+//     return arr[0]
+// }
+// console.log(firstElement([1, 2, 3, 4, 5])); // 1
+
+
+// -----------------ques 13------------
+// Write a function that takes an array and a number and returns the sum of first element and the number.
+
+// Your ES6 code here
+        //  const addingNumber=(arr=[],num)=>arr[0]+num
+        //   console.log(addingNumber([12,12,42,4],234))
+
+ 
+        // ----------------ques 14--------------
+
+        //Write a function that takes an array and returns the sum of first and last element.
+        
+        // Your ES6 code here
+
+        //  const sumFirstAndLast=(arr=[])=>arr[0]+arr[arr.length-1];
+        // console.log(sumFirstAndLast([1, 2, 3, 4, 8])); // 9
+
+        // --------------------ques 15--------------
+        // Write a function that takes an object representing a person's information (name, age, occupation) and returns a template literal that includes the person's name and age in a sentence.
+
+        // Your ES6 code here
+
+// const personInfo=(obj)=>`${obj.name } is ${obj.age} yrs old `
+
+
+// const person1 = { name: 'John', age: 25, occupation: 'Software Engineer' }
+// const person2 = { name: 'Jane', age: 45, occupation: 'Data Analyst' }
+// console.log(personInfo(person1)) // Expected output: "John is 25 years old."
+// console.log(personInfo(person2)) // Expected output: "Jane is 45 years old."
